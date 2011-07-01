9deb08f4c88a26dc597f96f53dc4c3af2a414807
Bertrand BRUN <bertrand.brun@me.com>
2011-07-01 11:46:34 +0200

    Nettoyage pour la version iPhone

--- src/com/dumbs/um2cal/models/Constant.java
+++
@@ -1,7 +0,0 @@
-package com.dumbs.um2cal.models;
-
-public class Constant {
-	public static final String APP_NAME = "UM2Cal";
-	public static final String EDT = "http://edt.ufr.univ-montp2.fr/php/ajax/";
-	public static final String PROGRAM = "PROGRAM";
-}
